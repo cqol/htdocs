af6236ad9605d1578dc3ba07604c9a88fd52c064
cqol_77 <cqol_77@126.com>
2013-12-14 00:24:12 +0800

    good night

--- jysm/modules/tshop-um-dp-ys/tshop-um-dp-ys.php
+++ jysm/modules/tshop-um-dp-ys/tshop-um-dp-ys.php
@@ -13,7 +13,24 @@
             <li class="pro_list">
                 <div class="pro_item">
                     <div class="img_warp">
-                        <a href="#" target="_blank" class="alink" style="background: url(../../assets/images/b_pro_01.jpg) no-repeat center center"></a>
+                        <a href="#" target="_blank" class="alink img_pop" style="background: url(../../assets/images/b_pro_01.jpg) no-repeat center center"></a>
+                        <div class="mark_box J_TWidget"  data-widget-type="Popup" data-widget-config="{
+                              'trigger':'.img_pop',
+                              'align':{
+                                      'node':'.img_pop',
+                                      'offset':[0,0],
+                                      'points':['lt','rb']
+                                      }
+                            }">
+                            <div class="mark"></div>
+
+                            <div class="ewm"></div>
+                            <div class="msg">
+                                <span class="sale">已售：<s>1888</s>件</span>
+                                <a href="#" target="_blank" class="gwc"></a>
+                                <a href="#" target="_blank" class="gd_like"></a>
+                            </div>
+                        </div>
                     </div>
                     <p class="pro_price">
                         <span class="price" style="">￥<span>888.00</span></span>
@@ -24,7 +41,7 @@
                     </a>
                     <div class="btn">
                         <?
-                           echo ''.gd_sc(0, 12) . gd_share(0, 1, 'cq', 12).'';
+                           echo gd_sc(0, 12) . gd_share(0, 1, 'cq', 12);
                         ?>
                         <a href="#" class="gd_buy" target="_blank">
                             点击进入
